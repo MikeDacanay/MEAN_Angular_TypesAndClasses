f26e5452ed1aa5271299c00d99865a8576fb9bc4
Mike Dacanay <migd64@gmail.com>
2017-09-13 21:48:30 -0700

    final commit, dealt with basic data types, object constructor, and basic functions

--- types_classes.ts
+++ types_classes.ts
@@ -7,6 +7,7 @@ var anythingVariable: any = 25;
 var arrayOne: boolean[] = [true, false, true];
 var arrayTwo: any[] = [true, 6, 'true'];
 var myObj: {[s: string]: number;} = {x: 5, y:10};
+
 class MyNode {
     val: number;
     _priv: number;
@@ -17,4 +18,16 @@ class MyNode {
     doSomething() {
         this._priv = 10;
     }
+}
+let myNodeInstance = new MyNode(10);
+
+console.log(myNodeInstance.val);
+
+function myFunction(): void{
+    console.log('Hello World');
+    return;
+}
+
+function sendingErrors(): never{
+    throw new Error ('Error message')
 }
